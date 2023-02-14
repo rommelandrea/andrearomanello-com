2e31f660ee49bce6388bdfb01e8792c4e21abcd2
Andrea Romanello aka rommel <rommelandrea@gmail.com>
2023-02-14 23:56:27 +0100

    setup basic config

--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,10 @@
 import type { SocialObjects } from "./types";
 
 export const SITE = {
-  website: "https://astro-paper.pages.dev/",
-  author: "Sat Naing",
+  website: "https://andrearomanello.com/",
+  author: "Andrea Romanello",
   desc: "A minimal, responsive and SEO-friendly Astro blog theme.",
-  title: "AstroPaper",
+  title: "Andrea Romanello",
   ogImage: "astropaper-og.jpg",
   lightAndDarkMode: true,
   postPerPage: 3,
@@ -20,7 +20,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://github.com/rommelandrea/astro-paper",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
@@ -28,17 +28,17 @@ export const SOCIALS: SocialObjects = [
     name: "Facebook",
     href: "https://github.com/satnaing/astro-paper",
     linkTitle: `${SITE.title} on Facebook`,
-    active: true,
+    active: false,
   },
   {
     name: "Instagram",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://instagram.com/rommelandrea",
     linkTitle: `${SITE.title} on Instagram`,
     active: true,
   },
   {
     name: "LinkedIn",
-    href: "https://github.com/satnaing/astro-paper",
+    href: "https://www.linkedin.com/in/andrea-romanello/",
     linkTitle: `${SITE.title} on LinkedIn`,
     active: true,
   },
