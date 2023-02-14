a4dfaa9919d22016c37c90139ecd7f3c8d83eeb7
Andrea Romanello aka rommel <rommelandrea@gmail.com>
2023-02-15 00:25:36 +0100

    fix some links

--- src/config.ts
+++ src/config.ts
@@ -20,7 +20,7 @@ export const LOGO_IMAGE = {
 export const SOCIALS: SocialObjects = [
   {
     name: "Github",
-    href: "https://github.com/rommelandrea/astro-paper",
+    href: "https://github.com/rommelandrea",
     linkTitle: ` ${SITE.title} on Github`,
     active: true,
   },
